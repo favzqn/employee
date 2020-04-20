ef1af9d9ab18d6b6dc78885f045d9f5ad9042f6f
Fauzan Fathurrahman <fauzan08fauzan@gmail.com>
2020-04-20 16:56:45 +0700

    Add Modal

--- screens/CreateEmployee.js
+++ screens/CreateEmployee.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { TextInput } from 'react-native-paper';
+import { StyleSheet, Text, View, Modal } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
 
 const CreateEmployee = () => {
     const [name, setName] = useState("")
@@ -45,6 +45,39 @@ const CreateEmployee = () => {
             mode="outlined"
             onChangeText={text => setSalary(text) }
             />
+            <Button style={styles.inputStyle} 
+            theme={theme}
+            icon="upload" mode="contained" onPress={() => setModal(true)}>
+                Upload Image
+            </Button>
+            <Button style={styles.inputStyle} 
+            theme={theme}
+            icon="content-save" mode="contained" onPress={() => setModal(true)}>
+                Save
+            </Button>
+            <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modal}
+            onRequestClose={()=>{
+                setModal(false)
+            }}
+            >
+                <View style={styles.modalView}>
+                    <View style={styles.modalButtonView}>
+                        <Button icon="camera" theme={theme} mode="contained" onPress={() => setModal(false)}>
+                            Camera
+                        </Button>
+
+                        <Button icon="image-area" theme={theme} mode="contained" onPress={() => setModal(false)}>
+                            Gallery
+                        </Button>
+                    </View>
+                    <Button theme={theme} onPress={() => setModal(false)}>
+                            Cancel
+                    </Button>
+                </View>
+            </Modal>
         </View>
     )
 }
@@ -61,6 +94,16 @@ const styles = StyleSheet.create({
     inputStyle:{
         marginVertical:4,
         marginHorizontal: 7
+    },
+    modalView:{
+        position:"absolute",
+        bottom:2,
+        width:"100%"
+    },
+    modalButtonView:{
+        flexDirection:"row",
+        justifyContent:"space-between",
+        padding:10
     }
 })
 
